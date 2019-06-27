4832eb3b62773369cc3299de072f1f99b5583185
Fredrik Noring <noring@nocrew.org>
2019-06-27 17:01:18 +0200

    MIPS: PS2: Define the PlayStation 2 platform
    Signed-off-by: Fredrik Noring <noring@nocrew.org>

---
+++ arch/mips/ps2/prom.c
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * PlayStation 2 PROM handling
+ */
+
+#include <linux/module.h>
+
+void prom_putchar(char c)
+{
+}
+
+void __init prom_init(void)
+{
+}
+
+void __init prom_free_prom_memory(void)
+{
+}
